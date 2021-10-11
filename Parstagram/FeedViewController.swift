c2c5ecf1d628d148d079aed2f8bd779d30c44c44
Luu <loluu@lib310392.ttu.edu>
2021-10-10 20:50:45 -0500

    finished

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -6,28 +6,65 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
 class FeedViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
     
     
-
+    var posts = [PFObject]()
     @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-
-        // Do any additional setup after loading the view.
     }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        // Do any additional setup after loading the view.
+    override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            
+            let query = PFQuery(className: "Posts")
+            query.includeKey("author")
+            query.limit = 20
+            query.order(byDescending: "createdAt")
+
+            query.findObjectsInBackground{(posts, error) in
+                if posts != nil{
+                    self.posts = posts!
+                    self.tableView.reloadData()
+                }
+            }
+            
+        }
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return posts.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+            let post = posts[indexPath.row]
+            let user = post["author"] as! PFUser
+            cell.usernameLable.text = user.username
+            cell.captionLable.text = post["caption"] as! String
+            
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            
+            cell.photoView.af_setImage(withURL: url)
+            
+            return cell
+            
+            
+        }
+    
+    @IBAction func onLogOut(_ sender: Any) {
+        PFUser.logOut()
+        var currentUser = PFUser.current()
+        dismiss(animated: true, completion: nil)
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-
     /*
     // MARK: - Navigation
 
